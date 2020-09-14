21184e51226403ec49f72def78457524f2b10978
Krishna <7krishna7dhakal7@gmail.com>
2020-09-14 13:52:12 +0545

    greedy rocks

---
+++ competitive/greedy.js
@@ -0,0 +1,58 @@
+//double shows time limit
+
+/* function minimumAbsoluteDifference(arr) {
+    var min =Math.max(...arr);
+   
+   for(var i =0;i<arr.length;i++){
+       for(var j=i+1;j<arr.length;j++){
+          
+           var k=Math.abs(arr[i]-arr[j]);
+            
+           if(k<min){
+               min=k;
+           }
+       
+
+
+       }
+   }
+   return min;
+
+}
+
+
+ */
+
+
+//not double but a greedy appraoch solves the problem
+
+
+function minimumAbsoluteDifference(arr) {
+    var min =Math.max(...arr);
+
+    function signCheck(a,b){
+        return a>0 == b>0 ? true : false;
+    }
+    
+    var m = arr.sort((a,b)=>a-b);
+    console.log(m)
+
+    for(var i =0; i < m.length;i++){
+        if(signCheck(m[i+1],m[i])){
+            if((m[i+1]-m[i])<min){
+                min=m[i+1]-m[i]
+            }
+        }else{
+            if((Math.abs(m[i])+Math.abs(m[i+1]))<min){
+                min=Math.abs(m[i])+Math.abs(m[i+1])
+            }
+        }
+        console.log(min)
+    }
+
+
+
+
+    return min;
+
+}
